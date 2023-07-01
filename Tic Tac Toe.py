d4b9f36426fefe7b83666a035f4c8c6aaa721b88
Jiří Zelenka <j3.zelenka@gmail.com>
2023-07-01 07:39:23 -0400

    docs: changed all the comments to English

--- Tic Tac Toe.py
+++ Tic Tac Toe.py
@@ -10,8 +10,6 @@ button_colour_inactive = "black"
 text_button_colour = "blue"
 frame_colour = "grey"
 title_colour = "white"
-# je třeba unifikovat title frame na obou stránkách, ať ho v každé nemusím zavádět zvlášť
-# je třeba vytvořit clasu na všechna tato jednotlivá nastavení
 
 players = ["X", "O"]
 player = random.choice(players)
@@ -23,49 +21,48 @@ class MainMenu:
         title_frame = tk.Frame(root, pady=10, bg=frame_colour)
         title_frame.pack()
 
-        # Tohle je můj hlavní frame, ve kterém vše leží
+        # Main menu fram
         self.title = tk.Label(title_frame, padx=10, pady=10, text="Main Menu", font=(font1, title_size), bg=title_colour)
         self.title.pack()
 
-        # Menu pro ukončení a restart 
+        # main menu window
         lista = tk.Menu(root)
-        root .config(menu=lista)
+        root.config(menu=lista)
         options_menu = tk.Menu(lista)
         lista.add_cascade(label="Options", menu=options_menu)
         options_menu.add_command(label="Restart")
         options_menu.add_command(label="Exit", command=root.quit)
        
-        # Grid pro tlačítka nastavující hru
+        # A grid for all the settings buttons, PvP, PvE, 3x3, 4x4
         buttonframe = tk.Frame(root, padx = 10, pady = 10, bg=frame_colour)
         buttonframe.columnconfigure(0, weight=1)
         buttonframe.columnconfigure(1, weight=1)
         buttonframe.place(relx=0.33, rely=.55, anchor="center")
         
-        # Tlačítko pro PVP
+        # A button for PvP
         self.pvp_button = tk.Button(buttonframe, width = 14, height=7,  text="PvP", font=(font1, font1_size), bg=button_colour,command=self.action0)
         self.pvp_button.grid(row=0, column=0, pady=5)
         
-        # Tlačítko pro PVE
+        # A button for PvE
         self.pve_button = tk.Button(buttonframe, width = 14, height=7, text="PvE", font=(font1, font1_size), bg=button_colour, command=self.action1)
         self.pve_button.grid(row=0, column=1, pady=5, padx=5)
 
-        # Tlačítko pro 3x3
+        # A button for 3x3
         self.grid3_button = tk.Button(buttonframe, width=14, height=7, text="3x3", font=(font1, font1_size), bg=button_colour, command=self.action2)
         self.grid3_button.grid(row=1, column=0, pady=5, padx=5)
 
-        # Tlačítko pro 4x4
+        # A button for 4x4
         self.grid4_button = tk.Button(buttonframe, width=14, height=7, text="4x4", font=(font1, font1_size), bg=button_colour, command=self.action3)
         self.grid4_button.grid(row=1, column=1, pady=5, padx=5)
 
         # Grid pro startovací tlačítko
         start_button_frame = tk.Frame(root, padx=10, pady=10, bg=frame_colour)
         start_button_frame.place(relx=.65, rely=.4)
-
-        # Startovací tlačítko
+        # Start button
         self.start_button = tk.Button(start_button_frame, width=14, height=7, text="START", font=(font1, font1_size), fg=text_button_colour,bg=button_colour, command=self.action4)
         self.start_button.grid(row=1, column=1, pady=5, padx=5)
 
-    # Jednotlivé funkce na změnu barvy + lze sem i přidat funkce na nastavení herního pole?
+    # action functions to configure what each button does
     def action0(self):
         self.pvp_button.configure(bg=button_colour_clicked)
         self.pve_button.configure(bg=button_colour_inactive)
