6f1ba638b831c4d73b74dcad611e013f3d4902e6
Jiří Zelenka <j3.zelenka@gmail.com>
2023-07-01 09:58:04 -0400

    fix: fixed some wrong Help object references in gameBoard.py

--- gameBoard.py
+++ gameBoard.py
@@ -3,10 +3,12 @@ from help import Help
 
 class TicTacToeBoard:
     def __init__(self, board):
-        title_frame = tk.Frame(board, pady=10, bg=Help.frame_colour)
+        h = Help()
+        self.h = h
+        title_frame = tk.Frame(board, pady=10, bg=h.frame_colour)
         title_frame.pack()
        
-        player_turn_label = tk.Label(title_frame, padx=10, pady=10, text=Help.player + " turn", font=(Help.font1, Help.title_size), bg=Help.title_colour)
+        player_turn_label = tk.Label(title_frame, padx=10, pady=10, text=h.player + " turn", font=(h.font1, h.title_size), bg=h.title_colour)
         player_turn_label.pack(side="top")
         self.create_board3(board)
 
@@ -16,5 +18,5 @@ class TicTacToeBoard:
         
         for row in range(3):
             for column in range(3):
-                Help.board3[row][column] = tk.Button(board_frame, text="", width=16, height=8)
-                Help.board3[row][column].grid(row=row, column=column)
+                self.h.board3[row][column] = tk.Button(board_frame, text="", width=16, height=8)
+                self.h.board3[row][column].grid(row=row, column=column)
--- main.py
+++ main.py
@@ -9,5 +9,5 @@ if __name__ == "__main__":
     window.title("Tic-Tac-Toe Game")
     window.configure(bg="grey")
     gui = MainMenu(window)
-    # gui = TicTacToeBoard(window2)
+    # gui = TicTacToeBoard(window)
     window.mainloop()
