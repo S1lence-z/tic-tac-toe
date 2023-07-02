dec7a1bc36d0ed29d40f8c0eccf66300fbe89ac1
Jiří Zelenka <j3.zelenka@gmail.com>
2023-07-02 19:07:59 -0400

    feat: added show() method to the MainMenu class

--- main.py
+++ main.py
@@ -14,18 +14,19 @@ class TicTacToeGame:
         # possible game states
         # all_possible_game_states = { mainMenu, inGame, endGame }
         self.current_game_state = "mainMenu"
-        self.start_game_status = False
-        menu = MainMenu(self.window, self.h)
-        game = GameBoard(self.window, self.h)
+        # game windows
+        self.mainMenu = MainMenu(self.window, self.h)
+        self.gameBoard = GameBoard(self.window, self.h)
 
     def gameLoop(self) -> None:
         if self.current_game_state == "mainMenu":
-            if (self.start_game_status):
+            self.mainMenu.show()
+            if (self.mainMenu.start_game_status):
                 self.current_game_state = "inGame"
         
         elif self.current_game_state == "inGame":
             print("more")
-        
+            
         elif self.current_game_state == "endGame":
             print("Not implemented yet")
             
--- mainMenu.py
+++ mainMenu.py
@@ -7,40 +7,36 @@ class MainMenu:
         self.create_title_frame(root, h)
         self.create_menu_bar(root)
         # create a frame and buttons to control game settings
-        button_frame = self.create_button_frame(root)
-        self.pvp_button = self.create_button(button_frame, "PvP", self.action0, 0, 0)
-        self.pve_button = self.create_button(button_frame, "PvE", self.action1, 0, 1)
-        self.grid3_button = self.create_button(button_frame, "3x3", self.action2, 1, 0)
-        self.grid4_button = self.create_button(button_frame, "4x4", self.action3, 1, 1)
+        self.button_frame = self.create_button_frame(root)
+        self.pvp_button = self.create_button(self.button_frame, "PvP", self.action0, 0, 0)
+        self.pve_button = self.create_button(self.button_frame, "PvE", self.action1, 0, 1)
+        self.grid3_button = self.create_button(self.button_frame, "3x3", self.action2, 1, 0)
+        self.grid4_button = self.create_button(self.button_frame, "4x4", self.action3, 1, 1)
         # create a frame and the start button
-        start_button_frame = self.create_start_button_frame(root)
-        self.start_button = self.create_start_button(start_button_frame, "START", 1, 1)
+        self.start_button_frame = self.create_start_button_frame(root)
+        self.start_button = self.create_start_button(self.start_button_frame, "START", 1, 1)
         # variables for the start button
         self.start_game_status = False
         
     def create_start_button_frame(self, root):
         start_button_frame = tk.Frame(root, padx=10, pady=10, bg=self.h.frame_colour)
-        start_button_frame.place(relx=.65, rely=.4)
         return start_button_frame
     
     def create_start_button(self, frame, text, row, column):
         start_button = tk.Button(frame, width=14, height=7, text=text, bg=self.h.start_button_colour)
         start_button.configure(font=(self.h.start_button_text_colour, self.h.button_text_size))
         start_button.configure(command=lambda: self.action4(start_button))
-        start_button.grid(row=row, column=column, pady=5, padx=5)
         return start_button
     
     def create_button_frame(self, root) -> None:
         button_frame = tk.Frame(root, padx = 10, pady = 10, bg=self.h.frame_colour)
         button_frame.columnconfigure(0, weight=1)
         button_frame.columnconfigure(1, weight=1)
-        button_frame.place(relx=0.33, rely=.55, anchor="center")
         return button_frame
         
     def create_button(self, frame, text, action, row, column):
         button = tk.Button(frame, width=14, height=7, text=text, bg=self.h.button_colour, command=action)
         button.configure(font=(self.h.button_text_font, self.h.button_text_size))
-        button.grid(row=row, column=column, pady=5, padx=5)
         return button
         
     def create_title_frame(self, root, h) -> None:
@@ -59,6 +55,16 @@ class MainMenu:
         bar.add_cascade(label="Options", menu=options_menu)
         options_menu.add_command(label="Restart")
         options_menu.add_command(label="Exit", command=root.quit)
+        
+    def show(self) -> None:
+        self.start_button_frame.place(relx=.65, rely=.4)
+        self.start_button.grid(row=1, column=1, pady=5, padx=5)
+        # other buttons
+        self.button_frame.place(relx=0.33, rely=.55, anchor="center")
+        self.pvp_button.grid(row=0, column=0, pady=5, padx=5)
+        self.pve_button.grid(row=0, column=1, pady=5, padx=5)
+        self.grid3_button.grid(row=1, column=0, pady=5, padx=5)
+        self.grid4_button.grid(row=1, column=1, pady=5, padx=5)
 
     # action functions to configure what each button does
     def action0(self) -> None:
