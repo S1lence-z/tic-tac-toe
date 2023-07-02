5e4461794574010b13714d6fc35948e04f623f7e
Jiří Zelenka <j3.zelenka@gmail.com>
2023-07-02 09:44:05 -0400

    feat: added a switch case to change game windows

--- main.py
+++ main.py
@@ -3,13 +3,36 @@ from help import Help
 from mainMenu import MainMenu
 from gameBoard import GameBoard
 
-if __name__ == "__main__":
+class TicTacToeGame:
+    # necessary classes and variables
     h = Help()
+    chosen_board_size = "3"
+    chosen_game_mode = "PvP"
+    # main tkinter window settings
     window = tk.Tk()
     window.minsize(600, 600)
     window.maxsize(600, 600)
     window.title("TicTacToe Game")
     window.configure(bg=h.frame_colour)
-    # gui = MainMenu(window, Help())
-    gui = GameBoard(window, Help())
-    window.mainloop()
+    # possible game states
+    possible_game_states = {
+        "mainMenu",
+        "inGame",
+        "endGame"
+    }
+    current_game_state = "mainMenu"
+    
+    def gameLoop(self) -> None:
+        match self.current_game_state:
+            case "mainMenu":
+                current_window = MainMenu(self.window, self.h)
+            case "inGame":
+                current_window = GameBoard(self.window, self.h)
+            case "endGame":
+                print("Not implemented yet")
+        
+        self.window.mainloop()
+
+if __name__ == "__main__":
+    game = TicTacToeGame()
+    game.gameLoop()
