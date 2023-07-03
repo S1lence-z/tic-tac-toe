b2d181c2c9b3892c347b8d90e7072c6489d133a9
Jiří Zelenka <j3.zelenka@gmail.com>
2023-07-02 22:24:32 -0400

    feat: the tiles change text according to which player is playing

--- gameBoard.py
+++ gameBoard.py
@@ -1,13 +1,19 @@
 import tkinter as tk
+from tkinter import messagebox
 from help import Help
 
 class GameBoard:
     def __init__(self, board, help: Help, game_controller):
+        # necessary classes and class variables
         self.h = help
         self.game_controller = game_controller
         self.board = board
+        # specific playe variables
+        self.player = help.player
+        self.current_player = self.player
+        # game board label
         self.title_frame = tk.Frame(board, pady=10, bg=help.frame_colour)
-        self.player_turn_label = tk.Label(self.title_frame, padx=10, pady=10, text=help.player + " turn", font=(help.button_text_font, help.title_size), bg=help.title_colour)
+        self.player_turn_label = tk.Label(self.title_frame, padx=10, pady=10, text=self.current_player + " turn", font=(help.button_text_font, help.title_size), bg=help.title_colour)
         self.board_widgets = []
         
     def end_game(self) -> None:
@@ -21,6 +27,7 @@ class GameBoard:
             for column in range(3):
                 widget = tk.Button(board_frame, text="", width=20, height=10)
                 widget.grid(row=row, column=column)
+                widget.configure(command=lambda clicked_button=widget: self.player_action(clicked_button))
                 self.board_widgets.append(widget)
 
     def create_board4(self, board):
@@ -47,3 +54,17 @@ class GameBoard:
         self.player_turn_label.pack_forget()
         for widget in self.board_widgets:
             widget.grid_forget()
+            
+    def player_switch(self) -> None:
+        if (self.current_player == "X"):
+            self.current_player = "O"
+        else:
+            self.current_player = "X"
+            
+    def player_action(self, clicked_button: tk.Button) -> None:
+        # check if this tile is not already taken
+        if (clicked_button.cget("text") == ""):
+            clicked_button.configure(text=self.current_player)
+            self.player_switch()
+        else:
+            messagebox.showwarning("Warning!", "This tile is already taken!")
