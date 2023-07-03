e3930f49a570d73d51f0e4cfd025a3e17e33059c
Jiří Zelenka <j3.zelenka@gmail.com>
2023-07-03 12:37:53 -0400

    docs: added proper documentation for the TicTacToeGame class

--- main.py
+++ main.py
@@ -7,8 +7,34 @@ from endScreen import EndScreen
 from appWindow import AppWindow
 
 class TicTacToeGame:
+    """
+    A class representing the Tic Tac Toe game.
+
+    Attributes:
+        h (Help): An instance of the Help class.
+        window (AppWindow): An instance of the AppWindow class.
+        mainMenu (MainMenu): An instance of the MainMenu class.
+        gameBoard (GameBoard): An instance of the GameBoard class.
+        endScreen (EndScreen): An instance of the EndScreen class.
+        chosen_board_size (str): The chosen size of the game board.
+        chosen_game_mode (str): The chosen game mode.
+
+    Methods:
+        __init__(): Initializes the TicTacToeGame class.
+        change_gameBoard_size(size: str) -> None: Changes the game board size.
+        change_game_mode(mode: str) -> None: Changes the game mode.
+        start_game() -> None: Starts the game.
+        end_screen(label_text) -> None: Displays the end screen.
+        restart_with_same_settings() -> None: Restarts the game with the same settings.
+        exit_to_menu() -> None: Exits to the main menu.
+    """
+
     def __init__(self):
-        # necessary classes
+        """
+        Initializes the TicTacToeGame class.
+
+        Creates instances of necessary classes and sets initial game variables.
+        """
         self.h = Help()
         self.window = AppWindow(self.h)
         self.mainMenu = MainMenu(self.window, self.h, self)
@@ -17,17 +43,34 @@ class TicTacToeGame:
         self.mainMenu.show()
         self.gameBoard.hide()
         self.endScreen.hide()
-        # game variables
         self.chosen_board_size = ""
         self.chosen_game_mode = ""
-    
+
     def change_gameBoard_size(self, size: str) -> None:
+        """
+        Changes the game board size.
+
+        Args:
+            size (str): The new size of the game board.
+        """
         self.chosen_board_size = size
-    
+
     def change_game_mode(self, mode: str) -> None:
+        """
+        Changes the game mode.
+
+        Args:
+            mode (str): The new game mode.
+        """
         self.chosen_game_mode = mode
-        
+
     def start_game(self) -> None:
+        """
+        Starts the game.
+
+        Displays the game board with the chosen board size.
+        Shows warning messages if game mode or board size is not chosen.
+        """
         if (self.chosen_game_mode == ""):
             messagebox.showwarning("Warning!", "You have to choose the game mode!")
             return
@@ -37,33 +80,42 @@ class TicTacToeGame:
         self.endScreen.hide()
         self.mainMenu.hide()
         self.gameBoard.show(self.chosen_board_size)
-        return
-    
+
     def end_screen(self, label_text) -> None:
+        """
+        Displays the end screen.
+
+        Args:
+            label_text: The text to be displayed on the end screen.
+        """
         self.gameBoard.hide()
         self.mainMenu.hide()
         self.endScreen.show(label_text)
-        
+
     def restart_with_same_settings(self) -> None:
-        # hide previous instances
+        """
+        Restarts the game with the same settings.
+
+        Hides previous instances, creates new instances, and resets chosen variables.
+        """
         self.gameBoard.destroy_widgets()
         self.endScreen.destroy_widgets()
-        # create new instances and reset chosen variables
         self.gameBoard = GameBoard(self.window, self.h, self)
         self.endScreen = EndScreen(self.window, self.h, self)
-        # default show settings
         self.start_game()
-        
+
     def exit_to_menu(self) -> None:
-        # hide previous instances
+        """
+        Exits to the main menu.
+
+        Hides previous instances, creates new instances, and resets chosen variables.
+        """
         self.mainMenu.destroy_widgets()
         self.gameBoard.destroy_widgets()
         self.endScreen.destroy_widgets()
-        # create new instances and reset chosen variables
         self.mainMenu = MainMenu(self.window, self.h, self)
         self.gameBoard = GameBoard(self.window, self.h, self)
         self.endScreen = EndScreen(self.window, self.h, self)
-        # default show settings
         self.mainMenu.show()
         self.gameBoard.hide()
         self.endScreen.hide()
