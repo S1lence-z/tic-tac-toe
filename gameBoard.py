cee38d9183567842045131a8be48a90cf0190419
Jiří Zelenka <j3.zelenka@gmail.com>
2023-07-03 12:28:40 -0400

    docs: added proper documentation for the GameBoard class

--- gameBoard.py
+++ gameBoard.py
@@ -3,29 +3,67 @@ from tkinter import messagebox
 from help import Help
 
 class GameBoard:
+    """
+    Represents the game board of the TicTacToe game.
+    
+    Attributes:
+        h (Help): An instance of the Help class that provides configuration settings.
+        board: The tkinter root object.
+        game_controller: The game controller object that controls the game flow.
+        board_frame: The frame that holds the game board.
+        player: The current player.
+        current_player: The player who has the current turn.
+        title_frame: The frame that holds the title label.
+        player_turn_label: The label that displays the current player's turn.
+        board_widgets: The list of buttons representing the game board.
+    """
+    
     def __init__(self, root, help: Help, game_controller):
-        # necessary classes and class variables
+        """
+        Initializes the GameBoard instance.
+        
+        Args:
+            root: The tkinter root object.
+            help (Help): An instance of the Help class that provides configuration settings.
+            game_controller: The game controller object that controls the game flow.
+        """
         self.h = help
         self.board = root
         self.game_controller = game_controller
         self.board_frame = self.create_board_frame(self.board)
-        # specific playe variables
         self.player = self.h.player
         self.current_player = self.player
-        # game board label
         self.title_frame = tk.Frame(root, pady=5, bg=help.frame_colour)
         self.player_turn_label = tk.Label(self.title_frame, padx=10, pady=10, text=self.current_player + "'s turn", font=(help.button_text_font, help.title_size), bg=help.title_colour)
         self.board_widgets = []
         
     def end_game(self) -> None:
+        """
+        Ends the game and displays the end screen.
+        """
         self.game_controller.end_screen()
         
     def create_board_frame(self, root) -> tk.Frame:
+        """
+        Creates and returns the game board frame.
+        
+        Args:
+            root: The tkinter root object.
+            
+        Returns:
+            The created game board frame.
+        """
         board_frame = tk.Frame(root, bg=self.h.frame_colour)
         board_frame.place(relx=0.5, rely=.56, anchor="center")
         return board_frame
 
     def create_board3(self, board_frame):
+        """
+        Creates the 3x3 game board.
+        
+        Args:
+            board_frame: The frame where the game board will be placed.
+        """
         for row in range(3):
             for column in range(3):
                 widget = tk.Button(board_frame, text="", width=6, height=3, font=(self.h.button_text_font, self.h.button_text_size))
@@ -34,6 +72,12 @@ class GameBoard:
                 self.board_widgets.append(widget)
 
     def create_board4(self, board_frame):
+        """
+        Creates the 4x4 game board.
+        
+        Args:
+            board_frame: The frame where the game board will be placed.
+        """
         for row in range(4):
             for column in range(4):
                 widget = tk.Button(board_frame, text="", width=5, height=2, font=(self.h.button_text_font, self.h.button_text_size))
@@ -42,15 +86,23 @@ class GameBoard:
                 self.board_widgets.append(widget)
 
     def show(self, chosen_board_size) -> None:
+        """
+        Displays the game board with the specified board size.
+        
+        Args:
+            chosen_board_size: The chosen board size (3 or 4).
+        """
         self.title_frame.pack()
         self.player_turn_label.pack(side="top")
-        # create chosen board
         if (chosen_board_size == "3"):
             self.create_board3(self.board_frame)
         elif (chosen_board_size == "4"):
             self.create_board4(self.board_frame)
 
     def hide(self):
+        """
+        Hides the game board.
+        """
         self.title_frame.pack_forget()
         self.player_turn_label.pack_forget()
         self.board_frame.pack_forget()
@@ -58,7 +110,9 @@ class GameBoard:
             widget.grid_forget()
             
     def destroy_widgets(self):
-        # Destroy all the widgets
+        """
+        Destroys all the widgets in the game board.
+        """
         self.title_frame.destroy()
         self.player_turn_label.destroy()
         self.board_frame.destroy()
@@ -66,14 +120,22 @@ class GameBoard:
             widget.destroy()
             
     def player_switch(self) -> None:
-        if (self.current_player == self.h.players[0]):
+        """
+        Switches the current player.
+        """
+        if self.current_player == self.h.players[0]:
             self.current_player = self.h.players[1]
         else:
             self.current_player = self.h.players[0]
             
     def player_action(self, clicked_button: tk.Button) -> None:
-        # check if this tile is not already taken
-        if (clicked_button.cget("text") == ""):
+        """
+        Performs the player's action when a button on the game board is clicked.
+        
+        Args:
+            clicked_button: The button that was clicked.
+        """
+        if clicked_button.cget("text") == "":
             clicked_button.configure(text=self.current_player)
             if (self.check_draw()):
                 self.game_controller.end_screen(self.h.game_tie_message)
@@ -86,13 +148,28 @@ class GameBoard:
             messagebox.showwarning("Warning!", "This tile is already taken!")
             
     def check_draw(self) -> bool:
+        """
+        Checks if the game has resulted in a draw.
+        
+        Returns:
+            True if the game is a draw, False otherwise.
+        """
         for widget in self.board_widgets:
             if widget.cget("text") == "":
                 return False
         return True
 
     def check_winner(self, board_size) -> bool:
-        if (board_size == "3"):
+        """
+        Checks if there is a winner in the game.
+        
+        Args:
+            board_size: The size of the game board (3 or 4).
+            
+        Returns:
+            True if there is a winner, False otherwise.
+        """
+        if board_size == "3":
             # Horizontal check
             for row in range(3):
                 if (self.board_widgets[row * 3]["text"] == 
