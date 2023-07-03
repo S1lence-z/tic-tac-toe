47e31cdfad35aab03de89e0f90cb7ed6c48c8966
Jiří Zelenka <j3.zelenka@gmail.com>
2023-07-03 12:22:00 -0400

    docs: added documentation for the AppWindow class

--- appWindow.py
+++ appWindow.py
@@ -2,7 +2,20 @@ import tkinter as tk
 from help import Help
 
 class AppWindow(tk.Tk):
+    """
+    Represents the main application window for the TicTacToe game.
+    
+    Attributes:
+        h (Help): An instance of the Help class that provides configuration settings.
+    """
+    
     def __init__(self, h: Help):
+        """
+        Initializes the AppWindow instance.
+        
+        Args:
+            h (Help): An instance of the Help class that provides configuration settings.
+        """
         super().__init__()
         # main tkinter window settings
         self.minsize(600, 600)
