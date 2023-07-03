8209a83045b2250d22c92ca782cbc895402ebd0d
Jiří Zelenka <j3.zelenka@gmail.com>
2023-07-02 20:51:59 -0400

    feat: added hide method to the GameBoard class

--- gameBoard.py
+++ gameBoard.py
@@ -6,18 +6,19 @@ class GameBoard:
         self.h = help
         self.board = board
         self.title_frame = tk.Frame(board, pady=10, bg=help.frame_colour)
-        
         self.player_turn_label = tk.Label(self.title_frame, padx=10, pady=10, text=help.player + " turn", font=(help.button_text_font, help.title_size), bg=help.title_colour)
+        self.board_widgets = []
 
     def create_board3(self, board):
         board_frame = tk.Frame(board)
         board_frame.place(relx=0.5, rely=.56, anchor="center")
-        
+
         for row in range(3):
             for column in range(3):
-                self.h.board3[row][column] = tk.Button(board_frame, text="", width=20, height=10)
-                self.h.board3[row][column].grid(row=row, column=column)
-                
+                widget = tk.Button(board_frame, text="", width=20, height=10)
+                widget.grid(row=row, column=column)
+                self.board_widgets.append(widget)
+
     def create_board4(self, board):
         board_frame = tk.Frame(board)
         board_frame.place(relx=0.5, rely=.56, anchor="center")
@@ -27,14 +28,21 @@ class GameBoard:
 
         for row in range(4):
             for column in range(4):
-                self.h.board4[row][column] = tk.Button(sub_frame, text="", width=18, height=7)
-                self.h.board4[row][column].grid(row=row, column=column)
-                
+                widget = tk.Button(sub_frame, text="", width=18, height=7)
+                widget.grid(row=row, column=column)
+                self.board_widgets.append(widget)
+
     def show(self, chosen_board) -> None:
         self.title_frame.pack()
         self.player_turn_label.pack(side="top")
         # create chosen board
-        if (chosen_board == "3"):
+        if chosen_board == "3":
             self.create_board3(self.board)
-        elif (chosen_board == "4"):
+        elif chosen_board == "4":
             self.create_board4(self.board)
+
+    def hide(self):
+        self.title_frame.pack_forget()
+        self.player_turn_label.pack_forget()
+        for widget in self.board_widgets:
+            widget.grid_forget()
