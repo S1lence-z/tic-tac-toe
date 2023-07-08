b8b0f7976d779bfc266d5c5c169a94111ef051ab
Jiří Zelenka <j3.zelenka@gmail.com>
2023-07-08 00:06:24 -0400

    feat: improved the minimax function, in theory works but returns the finished board to the main class

--- minimaxAlg.py
+++ minimaxAlg.py
@@ -5,66 +5,76 @@ def get_winner(board_widgets: tk.Button, board_size) -> str:
     if board_size == "3":
         # Horizontal check
         for row in range(3):
-            if (board_widgets[row * 3]["text"] == 
-                board_widgets[row * 3 + 1]["text"] == 
-                board_widgets[row * 3 + 2]["text"] != ""
+            if (board_widgets[row * 3] == 
+                board_widgets[row * 3 + 1] == 
+                board_widgets[row * 3 + 2] != ""
                 ):
-                return board_widgets[row]["text"]
+                return board_widgets[row]
         # Vertical check
         for column in range(3):
-            if (board_widgets[column]["text"] == 
-                board_widgets[column + 3]["text"] == 
-                board_widgets[column + 6]["text"] != ""
+            if (board_widgets[column] == 
+                board_widgets[column + 3] == 
+                board_widgets[column + 6] != ""
                 ):
-                return board_widgets[column]["text"]
+                return board_widgets[column]
         # Diagonal check
-        if (board_widgets[0]["text"] == 
-            board_widgets[4]["text"] == 
-            board_widgets[8]["text"] != ""
+        if (board_widgets[0] == 
+            board_widgets[4] == 
+            board_widgets[8] != ""
             ):
-            return board_widgets[0]["text"]
-        if (board_widgets[2]["text"] == 
-            board_widgets[4]["text"] == 
-            board_widgets[6]["text"] != ""
+            return board_widgets[0]
+        if (board_widgets[2] == 
+            board_widgets[4] == 
+            board_widgets[6] != ""
             ):
-            return board_widgets[2]["text"]
+            return board_widgets[2]
         return None
 
-def is_draw(board_widgets: tk.Button) -> bool:
-    for widget in board_widgets:
-        if widget.cget("text") == "":
+def is_draw(board: list) -> bool:
+    for text in board:
+        if text == "":
             return False
     return True
 
+def convert_to_board_widgets(board: list[str], board_widgets: list[tk.Button]) -> list[tk.Button]:
+    for i in range(len(board)):
+        board_widgets[i].configure(text=board[i])
+    return board_widgets
+
 def minimax(gameBoard_class, board_widgets, depth: int, is_maximizing: bool):
-    human_player = gameBoard_class.h.player1
-    ai_player = gameBoard_class.h.player2
+    board = [widget.cget("text") for widget in board_widgets]
     
-    if gameBoard_class.check_winner(board_widgets, gameBoard_class.game_controller.chosen_board_size):
-        winner = get_winner(board_widgets, gameBoard_class.game_controller.chosen_board_size)
-        if winner == ai_player:
-            return 1
-        elif winner == human_player:
-            return -1
-        elif is_draw(board_widgets):
-            return 0
-    
+    scores = {
+        gameBoard_class.h.player1: -1,
+        gameBoard_class.h.player2: 1,
+        "tie": 0
+    }
+
+    if get_winner(board, gameBoard_class.game_controller.chosen_board_size):
+        if is_maximizing:
+            return scores[gameBoard_class.h.player2]
+        else:
+            return scores[gameBoard_class.h.player1]
+    elif is_draw(board):
+        return scores["tie"]
+
     if is_maximizing:
         best_score = float("-inf")
-        for widget in board_widgets:
-            if widget.cget("text") == "":
-                widget.configure(text=ai_player)
-                score = minimax(gameBoard_class, board_widgets, depth + 1, False)
-                widget.configure(text="")
+        for i in range(len(board)):
+            if board[i] == "":
+                board[i] = gameBoard_class.h.player2
+                temp = convert_to_board_widgets(board, board_widgets)
+                score = minimax(gameBoard_class, temp, depth + 1, False)
+                board[i] = ""
                 best_score = max(score, best_score)
         return best_score
-    
     else:
         best_score = float("inf")
-        for widget in board_widgets:
-            if widget.cget("text") == "":
-                widget.configure(text=human_player)
-                score = minimax(gameBoard_class, board_widgets, depth + 1, True)
-                widget.configure(text="")
+        for i in range(len(board)):
+            if board[i] == "":
+                board[i] = gameBoard_class.h.player1
+                temp = convert_to_board_widgets(board, board_widgets)
+                score = minimax(gameBoard_class, temp, depth + 1, True)
+                board[i] = ""
                 best_score = min(score, best_score)
         return best_score
